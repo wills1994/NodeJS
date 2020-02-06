7f8be38a14bbd9af1e53919796cd4b72196719e1
Wilson Pinto Dsouza <wills1994@hotmail.com>
2020-02-06 14:27:37 +0100

    connection of mongodb

--- components/message/store.js
+++ components/message/store.js
@@ -1,17 +1,29 @@
-const list = [];
+const db = require("mongoose");
+const Model = require('./model');
 
-function getMessages(){
-    return list;
+db.Promise = global.Promise;
+const options = {
+    keepAlive: 1,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
+db.connect('mongodb+srv://willy:prueba1234@cluster0-02ibv.mongodb.net/telegram?retryWrites=true&w=majority', options)
+    .then(() =>console.log('DB connected'))
+    .catch((err) => {
+        console.log(err);
+    });
+
+function addMessage(message) {
+    const fullMessage = new Model(message);
+    fullMessage.save();
 }
 
-function addMessage(message){
-     list.push(message);
+async function getMessages(){
+    const fullMessage = await Model.find();
+    return fullMessage;
 }
 
 module.exports = {
     add:addMessage,
     Getlist:getMessages
-    //get
-    //update
-    //delete
 }
