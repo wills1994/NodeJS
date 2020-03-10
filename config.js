2ba055779e410277556c12516e808d0d69d09b86
Wilson Pinto Dsouza <wills1994@hotmail.com>
2020-03-10 19:09:14 +0100

    update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl : process.env.DB_URL || 'mongodb+srv://willy:prueba1234@cluster0-02ibv.mongodb.net/telegram?retryWrites=true&w=majority',
+    dbUrl : process.env.DB_URL || 'mongodb+srv://user:pass@cluster0-02ibv.mongodb.net/telegram?retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
     host:process.env.HOST || 'http://localhost',
     publicRoute:process.env.PUBLIC_ROUTE || '/app',
